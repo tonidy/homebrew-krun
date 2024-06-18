fd59e08968bdaf2502f556bb28fbe30165b4c952
Toni D <tonidy@outlook.com>
2024-06-18 14:01:57 +0700

    Update hash

--- Formula/krunvm.rb
+++ Formula/krunvm.rb
@@ -2,7 +2,7 @@ class Krunvm < Formula
   desc "Manage lightweight VMs created from OCI images"
   homepage "https://github.com/tonidy/krunvm"
   url "https://github.com/tonidy/krunvm/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "ae60a4badbc1d88aed552c5e8743dfbb27ab63b24cbcf793b98c5babaa121ba1"
+  sha256 "c600d35f330820d015818482b3c6311f94fbe63a340d03c5c7ff938cbf2e5bc4"
   license "Apache-2.0"
 
   bottle do
