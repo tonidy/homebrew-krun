3b0643c6116c2d6fbeef12c9dcafc683d3c4356e
Toni D <tonidy@outlook.com>
2024-06-18 13:55:01 +0700

    Update lib

--- Formula/krunvm.rb
+++ Formula/krunvm.rb
@@ -2,12 +2,12 @@ class Krunvm < Formula
   desc "Manage lightweight VMs created from OCI images"
   homepage "https://github.com/tonidy/krunvm"
   url "https://github.com/tonidy/krunvm/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "c600d35f330820d015818482b3c6311f94fbe63a340d03c5c7ff938cbf2e5bc4"
+  sha256 "ae60a4badbc1d88aed552c5e8743dfbb27ab63b24cbcf793b98c5babaa121ba1"
   license "Apache-2.0"
 
   bottle do
     root_url "https://raw.githubusercontent.com/tonidy/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_sonoma: "e4df1d7f3f0f8408f0057da28485cc741b3d1873e7c0e892bef8a81ccbb942aa"
+    sha256 cellar: :any, arm64_sonoma: "79a2e9b229c46f44bfd98e416b654d95f832f28801bd0c92738414c8c94c9863"
   end
 
   depends_on "asciidoctor" => :build
